686025828e479ceda2f59d4ad4a372bb96057097
Audax Faarentie Anchirinah <audax@localhost.localdomain>
2017-02-08 21:19:39 +0100

    Included descriptive comments in my code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 module.exports = {
-
+//Funtion to find Minimum and Maximum numbers in an Array
 findMinMax : function (arr){
 				if (Math.min(...arr) === Math.max(...arr)) return [Math.max(...arr)] ;
 	  			return [Math.min(...arr),Math.max(...arr)];
